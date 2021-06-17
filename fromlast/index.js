aedcafb528da51ba25c012e40b09a18ec400dde3
mohammad-w-momani <mohammad.w.momani@gmail.com>
2021-06-17 19:22:47 +0300

    fromLast

--- fromlast/index.js
+++ fromlast/index.js
@@ -1,11 +1,15 @@
-const { LinkedList } = require('./linkedlist')
-    // --- Directions
-    // Given a linked list, return the element n spaces
-    // from the last node in the list.  Do not call the 'size'
-    // method of the linked list.  Assume that n will always
-    // be less than the length of the list.
-    // --- Examples
-
+// --- Directions
+// Given a linked list, return the element n spaces
+// from the last node in the list.  Do not call the 'size'
+// method of the linked list.  Assume that n will always
+// be less than the length of the list.
+// --- Examples
+//    const list = new List();
+//    list.insertLast('a');
+//    list.insertLast('b');
+//    list.insertLast('c');
+//    list.insertLast('d');
+//    fromLast(list, 2).data // 'b'
 
 
 function fromLast(list, n) {
@@ -22,11 +26,5 @@ function fromLast(list, n) {
     }
     return slow
 }
-const list = new LinkedList();
-list.insertLast('a');
-list.insertLast('b');
-list.insertLast('c');
-list.insertLast('d');
-console.log(fromLast(list, 2).data); // 'b'
 
 module.exports = fromLast;
